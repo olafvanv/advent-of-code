b16a62a33f93559d407dcf61849d55d0954af175
olafvanv <olafvanv@gmail.com>
2025-12-12 20:31:52 +0100

    day 7

---
+++ days/07/part-1.js
@@ -0,0 +1,33 @@
+const lib = require('../../lib');
+
+lib.getInput('7').then(input => {
+  const grid = input.split('\n').map(r => r.split(''));
+
+  let answer = 0;
+
+  for(const [rowIndex, row] of grid.entries()) {
+    if(rowIndex === 0 || rowIndex === grid.length) continue;
+
+    const rowAbove = grid[rowIndex - 1];
+    const rowBelow = grid[rowIndex + 1];
+
+    for(const [colIndex, col] of row.entries()) {
+      if(rowAbove[colIndex] === 'S') {
+        grid[rowIndex][colIndex] = '|'
+        continue;
+      }
+
+      if(col === '^' && rowAbove[colIndex] === '|') {
+        grid[rowIndex][colIndex - 1] = '|';
+        grid[rowIndex][colIndex + 1] = '|';
+        answer++;
+      }
+
+      if (rowAbove[colIndex] === '|' && col !== '^') {
+        grid[rowIndex][colIndex] = '|';
+      }
+    }
+  }
+  console.log(answer)
+})
+
